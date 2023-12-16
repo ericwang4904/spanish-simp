42913aab82528595eec88ae1cc412c4ff9ba6a24
Eric Wang <eriwang@nuevaschool.org>
2023-12-15 20:32:05 -0800

    gi

--- src/app.py
+++ src/app.py
@@ -27,12 +27,6 @@ base_openai_params = [{
     'presence_penalty': 0,
 }] # list of dict insures that data typing stays when in data_editor
 
-
-# App
-@st.cache_data
-def load_model():
-    return TS("placeholder", params={})
-
 def load_data(ts: TS, url):
     load_button = st.button("Load Data Into Model (this will reset progress)")
     if load_button:
@@ -67,21 +61,22 @@ def simplify_selections(ts: TS, selection: list, openai_params: dict):
         ts.simplify(selection, openai_params)
 
 st.title("Yolanda's Spanish Simplifier")
-ts = load_model()
+if 'ts' not in st.session_state:
+    st.session_state.ts = TS("placeholder", params=dict(**base_params[0], **base_restart_params[0]))
 api_key = st.text_input(label="OpenAI API Key", type='password')
-load_api_key(ts, api_key)
+load_api_key(st.session_state.ts, api_key)
 
 st.header("News Website URL")
 url = st.text_input("Website URL", placeholder='https://google.com')
-load_data(ts, url)
+load_data(st.session_state.ts, url)
 
 st.header("Model Parameter that Require a Reset")
 reset_params = st.data_editor(base_restart_params)
-load_restart_params(ts, reset_params[0])
+load_restart_params(st.session_state.ts, reset_params[0])
 
 st.header("Model Parameters")
 params = st.data_editor(base_params)
-load_params(ts, params[0])
+load_params(st.session_state.ts, params[0])
 
 st.header("OpenAI Parameters")
 openai_params = st.data_editor(base_openai_params)
@@ -89,14 +84,14 @@ openai_params = st.data_editor(base_openai_params)
 st.header("Simplification Selection")
 col1, col2 = st.columns([0.2, 0.8])
 with col1:
-    selection = st.data_editor(pd.Series(data=[False] * ts.len_s_group_tokens_list, name="Selection"))
-    simplify_selections(ts, np.nonzero(selection)[0].tolist(), openai_params[0])
+    selection = st.data_editor(pd.Series(data=[False] * st.session_state.ts.len_s_group_tokens_list, name="Selection"))
+    simplify_selections(st.session_state.ts, np.nonzero(selection)[0].tolist(), openai_params[0])
 
 with col2:
     ui_df = pd.DataFrame(
     {   
-        "Original": ts.group_tokens,
-        "Simplified": ts.s_group_tokens,
+        "Original": st.session_state.ts.group_tokens,
+        "Simplified": st.session_state.ts.s_group_tokens,
     }
     )
     st.table(ui_df)
